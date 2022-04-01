f9017d2cacd300b6646747a69ae4a4d97c6ac29e
Jens Carlén <jens.carlen@offerta.se>
2022-04-01 15:34:49 +0200

    WIP

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,11 @@ def test_create_new_blog_post(client):
     assert response.status_code == 201
     assert response.json["id"] is not None
     assert response.json["type"] == "blog"
+
+
+def test_create_new_blog_post_and_read_it(client):
+    creation = client.post("/blog/", json={"title": "hey", "body": "baberiba"})
+    response = client.get(f"/blog/{creation.json['id']}")
+    assert response.status_code == 200
+    assert response.json["title"] == "hey"
+    assert response.json["body"] == "baberiba"
