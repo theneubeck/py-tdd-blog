60f6708556984fe05cada5b5bf26c46ba01b660d
Jens Carlén <jens.carlen@offerta.se>
2022-04-01 15:55:03 +0200

    WIP

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,36 @@
 # helpers
 def create_blog_post(client, body=None):
     body = body or {"title": "hey", "body": "baberiba"}
-    response = client.post("/blog/", json={"title": "hey", "body": "baberiba"})
+    response = client.post("/blog/", json=body)
     assert response.status_code == 201
     return response
 
 
 # tests
 def test_view_blog_posts(client):
-    create_blog_post(client, {"title": "hey", "body": "baberiba"})
+    creation = create_blog_post(client, {"title": "hey", "body": "baberiba"})
     response = client.get("/blog/")
     assert response.status_code == 200
-    assert response.json == {"posts": [{"title": "hey", "body": "baberiba"}]}
+    assert response.json == {"posts": [{"id": creation.json["id"], "type": "post", "title": "hey", "body": "baberiba"}]}
 
 
 def test_create_new_blog_post(client):
     response = create_blog_post(client, {"title": "hey", "body": "baberiba"})
     assert response.json["id"] is not None
-    assert response.json["type"] == "blog"
+    assert response.json["type"] == "post"
 
 
 def test_create_new_blog_post_and_read_it(client):
     creation = create_blog_post(client, {"title": "hey", "body": "baberiba"})
     response = client.get(f"/blog/{creation.json['id']}")
     assert response.status_code == 200
+    assert response.json["id"] is not None
+    assert response.json["type"] == "post"
     assert response.json["title"] == "hey"
     assert response.json["body"] == "baberiba"
+
+
+def test_create_with_missing_title(client):
+    response = client.post("/blog/", json={"body": "baberiba"})
+    assert response.status_code == 400
+    assert response.json["errors"] == [{"title": "is required"}]
