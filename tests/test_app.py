200f030d26c54650078176c96a0e568d0de6ffe1
Jens Carlén <jens.carlen@offerta.se>
2022-04-01 15:26:06 +0200

    WIP

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
-def test_request_example(client):
+def test_view_blog_posts(client):
     response = client.get("/blog/")
     assert response.status_code == 200
-    assert response.json == [{"posts": {"title": "My first POST"}}]
+    assert response.json == {"posts": [{"title": "My first POST"}]}
+
+
+def test_create_new_blog_post(client):
+    response = client.post("/blog/")
+    assert response.status_code == 201
+    assert response.json.id is not None
+    assert response.json.type == "blog"
