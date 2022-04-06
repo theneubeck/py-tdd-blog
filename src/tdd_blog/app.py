15a7645d6aa6ec1125f76ae31898da49aa9ed87c
Oscar Johansson <oscar.johansson@offerta.se>
2022-04-06 17:50:00 +0200

    SHACK

--- src/tdd_blog/app.py
+++ src/tdd_blog/app.py
@@ -2,10 +2,23 @@ import functools
 from . import db
 import json
 import uuid
+from typing import Dict
 
 from flask import Blueprint, jsonify, request
 
 bp = Blueprint("blog", __name__, url_prefix="/blog")
+
+
+def write_to_db(id: str, body: Dict) -> None:
+    db.set(id, body)
+
+
+def create_blog_post(body: Dict) -> str:
+    guid = str(uuid.uuid4())
+    body["id"] = guid
+    body["type"] = "post"
+    write_to_db(guid, body)
+    return guid
 
 
 @bp.route(
@@ -27,23 +40,29 @@ def blog_post():
             return {"errors": [{"body": "must be at least 50 chars"}]}, 400
 
         body = request.json
-        guid = str(uuid.uuid4())
-        body["id"] = guid
-        body["type"] = "post"
-        db.set(guid, body)
-        return {"id": guid, "type": "post"}, 201
+        return {"id": create_blog_post(body), "type": "post"}, 201
     return {"error": "method not allowed"}, 406
 
 
 @bp.route(
     "/<id>",
-    methods=("GET",),
+    methods=("GET","PUT"),
 )
 def blog_post_by_id(id: str):
     if request.method == "GET":
-        import sys
-        print(db.items(), file=sys.stderr)
-        print(id, file=sys.stderr)
-        print(type(id), file=sys.stderr)
         return db.get(id), 200
+    elif request.method == "PUT":
+        write_to_db(id, request.json)
+        return {}, 200
+    return {"error": "method not allowed"}, 406
+
+@bp.route(
+    "/post/<id>",
+    methods=("POST",),
+)
+def edit_blog_post_by_id(id: str):
+    if request.method == "POST":
+        body = request.json
+        write_to_db(id, body)
+
     return {"error": "method not allowed"}, 406
