b5c8827765b94d4ea8317730513f8f02a226ea79
Oscar Johansson <oscar.johansson@offerta.se>
2022-11-21 14:14:59 +0100

    SCHACK

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,6 @@ def test_create_post(client):
     response = client.post("/posts", json={"title": "First Post", "body": "Informative body"})
     assert response.status_code == 200
     assert response.json["id"] is not None
+
+    response2 = client.get(f"/posts/{response.json['id']}")
+    assert response2.status_code == 200
